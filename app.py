d6d53cddd6844918a638a889c5e0f102b10011da
Rajeshwar Bhakey <rbhakey@gmail.com>
2018-12-01 23:59:40 -0500

    First Version

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+print("Hello World!")
+import http.server
+import socketserver
 
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
